e49d182156a68ba2a2791d704de06d6b58133822
Yong Loon <nyl9488.yln@gmail.com>
2023-11-22 15:38:18 +0800

    Update FarmCardTemplate.tsx

--- components/FarmComponents/FarmCardTemplate/FarmCardTemplate.tsx
+++ components/FarmComponents/FarmCardTemplate/FarmCardTemplate.tsx
@@ -154,7 +154,7 @@ const FarmCardTemplate = (props: any) => {
       type: "entry_function_payload",
       function: `${moduleAddress2}::scripts::harvest`,
       type_arguments: ["0x9cc3c27b8d398ab6fc82cbc9dc6b43bb9164f72da465631628163822662a8580::lp_coin::LP<0xc0acbd3f0dc1d5361f8315e60fcbc577a41be51f049ca092ae6db7fa8609fab5::moon_coin::MoonCoin, 0x1::aptos_coin::AptosCoin, 0x45ef7a3a1221e7c10d190a550aa30fa5bc3208ed06ee3661ec0afa3d8b418580::curves::Uncorrelated>", "0x1::aptos_coin::AptosCoin"],
-      arguments: [moduleAddress2, earned],
+      arguments: [moduleAddress2],
     };
 
     try {
