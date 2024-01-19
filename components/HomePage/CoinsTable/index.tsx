8fc2ba4c383b7f0cbe2050e3787fe7f03648f86c
umar-khan25 <uk69200@gmail.com>
2024-01-19 18:55:19 +0500

    added

--- components/HomePage/CoinsTable/index.tsx
+++ components/HomePage/CoinsTable/index.tsx
@@ -152,7 +152,7 @@ const CoinsTable: FC = () => {
                       {coins?.metadata?.name}
                     </TableCell>
                     <TableCell>
-                      {coins?.amount / 10 ** 8}
+                      {coins?.amount / 10 ** coins?.metadata?.decimals}
                       <span>{coins?.metadata?.symbol}</span>
                     </TableCell>
                     <TableCell>{coins?.asset_type}</TableCell>
